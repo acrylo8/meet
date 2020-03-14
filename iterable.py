66dc30340b2aab74496aac358e779217684f4650
acrylo8 <61075866+acrylo8@users.noreply.github.com>
2020-03-14 12:29:03 +0530

    Update iterable.py

--- iterable.py
+++ iterable.py
@@ -1,10 +1,17 @@
-def iterable(obj):
+def pep8(args):
     try:
-        iter(obj)
-        return True
-    except TypeError:
-        return False
+        import pep8
+    except:
+        error('pep8 not found! Run "paver install_devtools".')
+        sys.exit(1)
 
-
-for element in [35, 34, [4, 5], (4, 5), {"a": 4}, "abcd", 4.5]:
-    print(element, "is iterable: ", iterable(element))
+    # Errors to ignore
+    ignore = ['E203', 'E121', 'E122', 'E123', 'E124', 'E125', 'E126', 'E127',
+        'E128', 'E402']
+    styleguide = pep8.StyleGuide(ignore=ignore,
+                                 exclude=['*/extlibs/*', '*/ext-src/*'],
+                                 repeat=True, max_line_length=79,
+                                 parse_argv=args)
+    styleguide.input_dir(options.plugin.source_dir)
+    info('===== PEP8 SUMMARY =====')
+    styleguide.options.report.print_statistics() 
